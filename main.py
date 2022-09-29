85fc0317944ebf9824e724ced02cca672ad294f2
Eugene Kos <mstrsid@live.com>
2022-09-29 11:45:29 +0300

    add grequests

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+from gevent import monkey as curious_george
+
+curious_george.patch_all(thread=False, select=False)
 import requests
+import grequests
 
 
 def get_card_curs():
-    r_cards_curs = requests.get('https://belarusbank.by/api/kurs_cards')
-    json_cards_curs = r_cards_curs.json()[0]
-    print(f'В {json_cards_curs["kurs_date_time"][:-9].replace("-", ".")} банк покупает | банк продает:')
+    urls = [
+        'https://belarusbank.by/api/kurs_cards',
+    ]
+    json_cards_curs = ''
+    rs = (grequests.get(u) for u in urls)
+    req = grequests.map(rs)
+    for response in req:
+        json_cards_curs = response.json()[len(response.json()) - 1]
+    print(f'В {json_cards_curs["kurs_date_time"].replace("-", ".")} банк покупает | банк продает:')
     print(f'USD: {json_cards_curs["USDCARD_in"]} | {json_cards_curs["USDCARD_out"]}\n'
           f'EUR: {json_cards_curs["EURCARD_in"]} | {json_cards_curs["EURCARD_out"]}\n'
           f'RUB: {json_cards_curs["RUBCARD_in"]} | {json_cards_curs["RUBCARD_out"]}')
